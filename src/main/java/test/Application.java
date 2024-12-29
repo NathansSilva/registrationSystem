ba1f04ac36e25164af56c5c298f6cf602ad9e4ee
Nathan <Nathansilva18@outlook.com>
2024-12-29 19:15:01 -0300

    Finalização aplicação

--- src/main/java/test/Application.java
+++ src/main/java/test/Application.java
@@ -2,21 +2,60 @@ package test;
 
 import domain.QuestionAnalyzer;
 import domain.QuestionReader;
-import domain.QuestionReaderTxt;
+import domain.QuestionReaderText;
+import domain.QuestionService;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
 
-        QuestionReader questionReader = new QuestionReaderTxt();
-        QuestionAnalyzer questionAnalyzer = new QuestionAnalyzer();
 
-        try {
-            questionAnalyzer.analyze("formulario.txt", questionReader);
-        } catch (IOException e) {
-            System.out.println("Não foi possível ler o arquivo. " + e.getMessage());
+        Scanner sc = new Scanner(System.in);
+        String path = "formulario.txt";
+        QuestionService questionService = new QuestionService(path);
+
+        while (true) {
+            System.out.println("1. List Questions");
+            System.out.println("2. Register new user");
+            System.out.println("3. List Users");
+            System.out.println("4. Add new question");
+            System.out.println("5. Remove question");
+            System.out.println("6. Exit");
+            System.out.print("Choose an option: ");
+
+            int option = Integer.parseInt(sc.nextLine());
+
+            switch (option) {
+                case 1:
+                    questionService.listQuestions();
+                    break;
+                case 2:
+                    questionService.registerUsers(sc);
+                    break;
+                case 3:
+                    questionService.listUsers();
+                    break;
+                case 4:
+                    System.out.print("Type the new question: ");
+                    String newQuestion = sc.nextLine();
+                    questionService.addQuestions(newQuestion);
+                    break;
+                case 5:
+                    System.out.print("Type the number of the question to be removed: ");
+                    int number = Integer.parseInt(sc.nextLine());
+                    questionService.removeQuestion(number);
+                    break;
+                case 6:
+                    System.out.println("Closing...");
+                    sc.close();
+                    return;
+                default:
+                    System.out.println("Invalid option.");
+            }
         }
+
 
 
     }
