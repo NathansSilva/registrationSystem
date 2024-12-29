cab249b38d2a69a8b51dc04e4a01c18473023d4a
Nathan <Nathansilva18@outlook.com>
2024-12-29 19:14:21 -0300

    Alteracao nome QuestionReaderText

--- src/main/java/domain/QuestionReaderText.java
+++ src/main/java/domain/QuestionReaderText.java
@@ -1,20 +1,21 @@
 package domain;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class QuestionReaderTxt implements QuestionReader {
+public class QuestionReaderText implements QuestionReader {
 
-    @Override
     public Question readQuestionsFromFile(String line) {
         String[] columns = line.split("-", 2);
         int number = Integer.parseInt(columns[0].trim());
-        String text = columns[1];
+        String text = columns[1].trim();
 
         return new Question(number, text);
     }
 
-    @Override
     public List<Question> readLinesFromQuestion(List<String> lines) {
         List<Question> questions = new ArrayList<>();
 
