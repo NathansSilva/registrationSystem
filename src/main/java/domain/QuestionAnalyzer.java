83c0137fa6ffe251cc888fa7f6061947cd4f4306
Nathan <Nathansilva18@outlook.com>
2024-12-29 19:13:54 -0300

    QuestionAnalyzer (faz a leitura das linhas)

--- src/main/java/domain/QuestionAnalyzer.java
+++ src/main/java/domain/QuestionAnalyzer.java
@@ -4,20 +4,27 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class QuestionAnalyzer {
 
     private static final String RESOURCES = "src/main/resources";
 
-    public void analyze(String filename, QuestionReader questionReader) throws IOException {
+    public List<Question> analyze(String filename, QuestionReader questionReader) throws IOException {
         Path path = Paths.get(RESOURCES, filename);
         List<String> lines = Files.readAllLines(path);
 
-        List<Question> questions = questionReader.readLinesFromQuestion(lines);
-
-        for (Question question : questions) {
-            System.out.println(question);
-        }
+        return questionReader.readLinesFromQuestion(lines);
     }
+
+    public static void writeFileTxt(String path, String content) {
+        try {
+            Files.write(Paths.get(path), content.getBytes(), StandardOpenOption.CREATE);
+        } catch (IOException e) {
+            System.out.println("Failed to write file. " + e.getMessage());
+        }
+
+    }
+
 }
